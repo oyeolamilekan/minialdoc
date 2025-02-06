f971b2bba2ebcc1097bf8cae82981fc6b5f702cd
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2025-02-06 20:45:05 +0100

    chore: refactored how redirect works

--- frontend/app/[domain]/page.tsx
+++ frontend/app/[domain]/page.tsx
@@ -1,14 +1,17 @@
 "use client"
 
 import { redirectToReference } from '@/lib/redirectToReference'
-import { useParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
 import React, { useEffect } from 'react'
 
 export default function Page() {
   const { domain } = useParams()
-
+  const router = useRouter()
+  
   useEffect(() => {
-    redirectToReference(domain as string)
+    redirectToReference(domain as string).then((slug) => {
+      router.push(`/reference/${slug}`)
+    })
   }, [domain])
 
   return (
--- frontend/lib/redirectToReference.ts
+++ frontend/lib/redirectToReference.ts
@@ -5,10 +5,7 @@ export async function redirectToReference(domain: string) {
   try {
     const response = await fetchProjectWithEndpoint(domain)
     const slug = response.data?.slug
-    
-    if (slug) {
-      window.location.replace(`/reference/${slug}`)
-    }
+    return slug
   } catch (error) {
     console.error('Failed to redirect to reference:', error)
   }
