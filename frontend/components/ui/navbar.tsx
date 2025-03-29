b8823f81e032edd3f2edccc180a167ccff7b8354
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2025-03-29 15:01:18 +0100

    chore: fix issues

--- frontend/components/ui/navbar.tsx
+++ frontend/components/ui/navbar.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
-import { Menu, X, Github } from 'lucide-react'
+import { Menu, X } from 'lucide-react'
 
 export function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
